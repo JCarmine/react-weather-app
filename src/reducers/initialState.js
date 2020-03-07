c9f7d3cfec2f7923e7999f03bc880e1c9a52413d
Jesse Carmine <jesse@revup.com>
2020-03-07 14:07:02 -0800

    Remove API creds from store for clarity; just hardcode them into the action.

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -2,8 +2,6 @@ import { fromJS } from 'immutable';
 
 const initialState = fromJS({
   weatherApp: {
-    apiPath: 'api.openweathermap.org/data/2.5/weather',
-    apiKey: '9f37eae0ba52698e13ef6b9efcf42e37',
     loading: false,
     error: false,
     data: null
